424c488862ebf08fe279095118c6a2ceec81f2e5
Thomas Fournier <thomasfournierbrest@gmail.com>
2024-04-28 23:26:32 +0200

    add style to forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,7 +5,11 @@ from accounts.models import CustomUser
 
 class UserForm(forms.ModelForm):
     # password input pour cacher notre mot de passe lors de la saisie
-    password = forms.CharField(widget=forms.PasswordInput())
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'block w-full ring-1 ring-UIElementBorderAndFocusRings hover:border-HoveredUIElementBorder shadow-sm py-3 px-4 border-UIElementBorderAndFocusRings rounded-md'}))
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'block w-full ring-1 ring-UIElementBorderAndFocusRings hover:border-HoveredUIElementBorder shadow-sm py-3 px-4 border-UIElementBorderAndFocusRings rounded-md'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'block w-full ring-1 ring-UIElementBorderAndFocusRings hover:border-HoveredUIElementBorder shadow-sm py-3 px-4 border-UIElementBorderAndFocusRings rounded-md'}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={'class': 'block w-full ring-1 ring-UIElementBorderAndFocusRings hover:border-HoveredUIElementBorder shadow-sm py-3 px-4 border-UIElementBorderAndFocusRings rounded-md'}))
+
 
     class Meta:
         model = CustomUser
