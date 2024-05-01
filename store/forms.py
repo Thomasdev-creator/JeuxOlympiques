6a218b62c3170874b801d62284a2783822d29ea6
Thomas Fournier <thomasfournierbrest@gmail.com>
2024-05-01 22:40:58 +0200

    add style to profile

--- store/forms.py
+++ store/forms.py
@@ -5,10 +5,11 @@ from store.models import Order
 
 class OrderForm(forms.ModelForm):
     # On boucle sur un nombre de 1 à 10
-    quantity = forms.ChoiceField(choices=[(i, i) for i in range(1, 11)])
+    quantity = forms.ChoiceField(choices=[(i, i) for i in range(1, 11)], widget=forms.Select(attrs={'class': 'block ring-1 ring-UIElementBorderAndFocusRings hover:border-HoveredUIElementBorder shadow-sm py-1 px-2 border-UIElementBorderAndFocusRings rounded-md'}))
     delete = forms.BooleanField(initial=False,
                                 required=False,
-                                label="Supprimer")
+                                label="Supprimer",
+                                widget=forms.CheckboxInput(attrs={'class': 'ring-1 ring-UIElementBorderAndFocus hoer:border-HoveredUIElementBorder shadow-sm py-1 px-2 border-UIElementBorderAndFocusRings rounded-md'}))
 
     # On lie notre formulaire au modèle order
     class Meta:
